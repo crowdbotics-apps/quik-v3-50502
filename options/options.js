629dddd32c02bb5aed7f22b4f87e49ed4f14bfeb
Crowdbotics <team@crowdbotics.com>
2024-12-24 05:20:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quik-v3-50502",
+  url: "https://quik-v3-50502.botics.co",
+  api: "https://quik-v3-50502.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
